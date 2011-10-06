8ea198d1671269020f324b5ed9d934d7f481a6c9
Jan Wielemaker <J.Wielemaker@cs.vu.nl>
2011-10-06 13:29:08 +0200

    Updated for local installation

--- unixScripts/unixVariables.sh
+++ unixScripts/unixVariables.sh
@@ -1,5 +1,5 @@
 # This is a typical environment for Linux, see elsewhere for Mac OS X
-JAVA_BIN=/usr/java/j2sdk1.4.2_09/bin
+JAVA_BIN=/usr/lib/jvm/java-6-openjdk/bin
 XSB_BIN_DIRECTORY=/home/YOUR_NAME/XSB271/config/i686-pc-linux-gnu/bin
-SWI_BIN_DIRECTORY=/usr/lib/pl-5.4.7/bin/i686-linux
+SWI_BIN_DIRECTORY=/home/jan/bin
 YAP_BIN_DIRECTORY=/usr/bin
