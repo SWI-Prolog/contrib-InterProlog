647ae1a1b450a110d5f9ab7ba7bb8777753964b9
Jan Wielemaker <J.Wielemaker@cs.vu.nl>
2011-10-06 13:28:39 +0200

    Fixed: path issues (swipl)

--- unixScripts/runSWISPListener.sh
+++ unixScripts/runSWISPListener.sh
@@ -1,3 +1,2 @@
 . unixVariables.sh
-${JAVA_BIN}/java -classpath ${CLASSPATH}:../interprolog.jar com.declarativa.interprolog.gui.SWISubprocessEngineWindow $1 ${SWI_BIN_DIRECTORY}/pl
-#For Mac version use /swipl rather than /pl
+${JAVA_BIN}/java -classpath ${CLASSPATH}:../interprolog.jar com.declarativa.interprolog.gui.SWISubprocessEngineWindow $1 ${SWI_BIN_DIRECTORY}/swipl
