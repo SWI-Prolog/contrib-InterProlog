60542f333816ddbb3f68c8e52190d7e6f58d5990
Jan Wielemaker <J.Wielemaker@cs.vu.nl>
2011-10-07 16:07:30 +0200

    Make sure we can use this, even if XSB is not present

--- unixScripts/compile_Ps.sh
+++ unixScripts/compile_Ps.sh
@@ -1,2 +1,7 @@
 cd ..
-$1/xsb -e "compile('com/declarativa/interprolog/xsb/interprolog.P'), compile('com/declarativa/interprolog/gui/visualization.P'), compile('com/declarativa/interprolog/tests.P'), halt."
+
+if [ -x "$1/xsb" ]; then
+  $1/xsb -e "compile('com/declarativa/interprolog/xsb/interprolog.P'), compile('com/declarativa/interprolog/gui/visualization.P'), compile('com/declarativa/interprolog/tests.P'), halt."
+else
+  echo "WARNING: no XSB; skipped compilation of XSB Prolog files"
+fi
